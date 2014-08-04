bdb79438dd555522f7a91ce613b209aa950f4f9c
odil-akhmedov <akhmedoff.odil@gmail.com>
2014-08-03 22:29:48 -0400

    That error with sms authentificatio

--- TheCommunity/Properties/AssemblyInfo.cs
+++ TheCommunity/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f9aad991-f293-40cc-b7bd-dadf56b1f10a")]
+[assembly: Guid("392728e3-eab7-40bb-ae06-47160398cee2")]
 
 // Version information for an assembly consists of the following four values:
 //
