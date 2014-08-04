780ce5036a951ccf7b4fcea389deff77a3734866
odil-akhmedov <akhmedoff.odil@gmail.com>
2014-08-03 21:44:37 -0400

    Setting up the project

---
+++ TheCommunity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheCommunity.Startup))]
+namespace TheCommunity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
